ff5ef5dc03e64e5ec2f0704880618479220cee0e
Scofield Liu <lcx.seima@gmail.com>
2026-07-08 16:55:06 +0800

    fix(admin): 设置 antd 简体中文语言包

--- apps/admin/src/app/providers.tsx
+++ apps/admin/src/app/providers.tsx
@@ -1,10 +1,11 @@
 import { App as AntdApp, ConfigProvider } from "antd";
+import zhCN from "antd/locale/zh_CN";
 import { type PropsWithChildren } from "react";
 import { theme } from "./theme.js";
 
 export function AppProviders({ children }: PropsWithChildren) {
   return (
-    <ConfigProvider theme={theme}>
+    <ConfigProvider locale={zhCN} theme={theme}>
       <AntdApp>{children}</AntdApp>
     </ConfigProvider>
   );
